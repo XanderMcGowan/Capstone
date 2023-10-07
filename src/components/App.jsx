b3dbbf44d97f49bfa0871fbc2c7df739ed1671fa
unknown <xander.mcgowan@gmail.com>
2023-10-07 18:05:11 -0500

    added login and register

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,17 @@
-import { useState } from 'react'
-import { nanoid } from 'nanoid'
-import ComposeForm from './ComposeForm'
-import Timeline from './Timeline'
-// import { FaTwitter } from 'react-icons/fa'
+import React from 'react';
+
 import './App.css'
-
-import initialTweets from '../tweets.json'
-
-
-const CURRENT_USER = 'nicotsou'
+import Router from '../Router'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  
-  const [tweets, setTweets] = useState(initialTweets)
-
-  
-  const handlePostTweet = (content) => {
-    const newTweet = {
-      content,
-      id: nanoid(),
-      country: "",
-      user: CURRENT_USER,
-      comments_count: 0,
-      retweets_count: 0,
-      favorites_count: 0,
-    }
-
-    setTweets([...tweets, newTweet])
-  }
-
   return (
-    <div className="app">
-      <ComposeForm onSubmit={handlePostTweet} />
-      <div className="separator"></div>
-      <Timeline tweets={tweets} />
-    </div>
-  )
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-import './index.css'
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
 
-
-
-
-// We use <App /> as the root React component to render
-ReactDOM.render(<App />, document.getElementById('root'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
